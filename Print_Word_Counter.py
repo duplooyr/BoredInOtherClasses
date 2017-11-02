a3c16218b7663cfed7f47ac1b75cb3e5c3c9b86e
Ryan Du Plooy <007Tropix@gmail.com>
2017-11-02 13:52:48 -0600

    Added times_looped

--- Print_Word_Counter.py
+++ Print_Word_Counter.py
@@ -1,5 +1,6 @@
-word_inputed = raw_input('What word would you like printed 10 times? ')
+times_looped = input('How many times would you liked your word looped? ')
+word_inputed = raw_input('What word would you like printed? ')
 x = 1
-for count in range(10):
+for count in range(times_looped):
     print ("%s #%d" % (word_inputed, x))
     x = x + 1
